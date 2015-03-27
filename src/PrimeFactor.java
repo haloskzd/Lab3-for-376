1cbac0dcd6a714a02cad5ab49c5d88fdd2dce78f
Zachary Haloski <zacharyhaloski@yahoo.com>
2015-03-27 19:41:02 -0400

    Passing test 3

--- src/PrimeFactor.java
+++ src/PrimeFactor.java
@@ -5,8 +5,10 @@ public class PrimeFactor {
 	
 	public static ArrayList<Integer> generate (int inputnum) {
 		ArrayList<Integer> list = new ArrayList<Integer>();
-		if(inputnum > 1) {
+		if(inputnum == 2) {
 			list.add(2);
+		} else if (inputnum == 3) {
+			list.add(3);
 		}
 		return list;
 	}
