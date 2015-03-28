04b29ae50e5cf0bae844564bd45920b692ce5624
Zachary Haloski <zacharyhaloski@yahoo.com>
2015-03-27 20:32:15 -0400

    Accidentally added the harder test and algorithm
    at the same time.  Must have missed commit, sorry.

--- src/PrimeFactorGenerator.java
+++ src/PrimeFactorGenerator.java
@@ -4,14 +4,21 @@ public class PrimeFactorGenerator {
 
 	public static ArrayList<Integer> generateList(int maxNum) {
 		ArrayList<Integer> retList = new ArrayList<Integer>();
-		if (maxNum > 3) {
-			retList.add(2);
-			retList.add(3);
-		} 
-		if (maxNum > 9) {
-			retList.add(5);
-			retList.add(7);
-		}
+		// loop through the numbers one by one
+				for (int i = 1; i < maxNum; i++) {
+					if (i == 2){
+						retList.add(i);
+					}
+					for (int j = 2; j < i; j++) {
+						if (j == i - 1) {
+							retList.add(i);
+						}
+						if (i % j == 0) {
+							break; // exit the inner for loop
+						}
+					}
+				}
+
 		return retList;
 	}
 
--- src/PrimeGeneratorTest.java
+++ src/PrimeGeneratorTest.java
@@ -29,4 +29,36 @@ public class PrimeGeneratorTest {
 		assertEquals(list, PrimeFactorGenerator.generateList(10));
 	}
 	
+	@Test
+	public void testNum100() {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(2);
+		list.add(3);
+		list.add(5);
+		list.add(7);
+		list.add(11);
+		list.add(13);
+		list.add(17);
+		list.add(19);
+		list.add(23);
+		list.add(29);
+		list.add(31);
+		list.add(37);
+		list.add(41);
+		list.add(43);
+		list.add(47);
+		list.add(53);
+		list.add(59);
+		list.add(61);
+		list.add(67);
+		list.add(71);
+		list.add(73);
+		list.add(79);
+		list.add(83);
+		list.add(89);
+		list.add(97);
+		
+		assertEquals(list, PrimeFactorGenerator.generateList(100));
+	}
+	
 }
