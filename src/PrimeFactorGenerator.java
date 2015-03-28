cfb25d35c3ee6c73b244700ce9179aa71d4c2196
Zachary Haloski <zacharyhaloski@yahoo.com>
2015-03-27 20:15:18 -0400

    Passing first PrimeGeneratorTest

---
+++ src/PrimeFactorGenerator.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+
+public class PrimeFactorGenerator {
+
+	public static ArrayList<Integer> generateList(int maxNum) {
+		// TODO Auto-generated method stub
+		return new ArrayList<Integer>();
+	}
+
+}
