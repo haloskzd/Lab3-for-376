96b2185cd71521426296641f8b1123c0c5bc1e9a
Zachary Haloski <zacharyhaloski@yahoo.com>
2015-03-27 20:22:09 -0400

    Passing the third test case

--- src/PrimeFactorGenerator.java
+++ src/PrimeFactorGenerator.java
@@ -4,9 +4,13 @@ public class PrimeFactorGenerator {
 
 	public static ArrayList<Integer> generateList(int maxNum) {
 		ArrayList<Integer> retList = new ArrayList<Integer>();
-		if (maxNum == 4) {
+		if (maxNum > 3) {
 			retList.add(2);
 			retList.add(3);
+		} 
+		if (maxNum > 9) {
+			retList.add(5);
+			retList.add(7);
 		}
 		return retList;
 	}
