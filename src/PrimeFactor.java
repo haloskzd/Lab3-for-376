fde65a3b1ee57d3b9fea9d69d317e1aa76adfd2e
Zachary Haloski <zacharyhaloski@yahoo.com>
2015-03-27 20:11:21 -0400

    Finished implementation

--- src/PrimeFactor.java
+++ src/PrimeFactor.java
@@ -5,24 +5,11 @@ public class PrimeFactor {
 	
 	public static ArrayList<Integer> generate (int inputnum) {
 		ArrayList<Integer> list = new ArrayList<Integer>();
-		if(inputnum == 2) {
-			list.add(2);
-		} else if (inputnum == 3) {
-			list.add(3);
-		} else if (inputnum == 4) {
-			list.add(2);
-			list.add(2);
-		} else if (inputnum == 6) {
-			list.add(2);
-			list.add(3);
-		} else if (inputnum == 8) {
-			list.add(2);
-			list.add(2);
-			list.add(2);
-		} else if (inputnum == 8) {
-			list.add(2);
-			list.add(2);
-			list.add(2);
+		for (int i = 2; i <= inputnum; i++) {
+			while (inputnum % i == 0) {
+		    	list.add(i);
+		       	inputnum /= i;
+		    }
 		}
 		return list;
 	}
